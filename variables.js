8b965550872a868acbe611106a0c0e919334538c
samportillo <hello@samportillo.com>
2015-12-28 14:08:46 -0600

    New Js

---
+++ variables.js
@@ -0,0 +1,20 @@
+var favColor = 'blue';
+var	myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName:  'sam',
+	lastName:  'armstrong',
+	favColors:  ['blue', 'red', 'green'],
+	yearsAlive : 100,
+	isMale : true
+};
+
+var whatIsMyFavColor = function (){
+	return 'blue';
+
+}
+
+var doSomething = function (){
+	console.log ("do something!!");
+}
